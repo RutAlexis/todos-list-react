e4b24226ed86add77ba778db0155622938316c83
Aleksandra Rutkowska <adrabikm@gmail.com>
2023-02-07 18:17:41 +0100

    added return to Form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,7 +1,7 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./style.css";
 
-const Form = ({addNewTask}) => {
+const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
 
     const onFromSubmit = (event) => {
@@ -9,16 +9,17 @@ const Form = ({addNewTask}) => {
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
     };
-
-    <form className="form" onSubmit={onFromSubmit}>
-        <input 
-        value = {newTaskContent}
-        className="form__input" 
-        placeholder="Co jest do zrobienia?" 
-        onChange={({target}) => setNewTaskContent(target.value)}
-        />
-        <button className="form__button">Dodaj zadanie</button>
-    </form>
+    return (
+        <form className="form" onSubmit={onFromSubmit}>
+            <input
+                value={newTaskContent}
+                className="form__input"
+                placeholder="Co jest do zrobienia?"
+                onChange={({ target }) => setNewTaskContent(target.value)}
+            />
+            <button className="form__button">Dodaj zadanie</button>
+        </form>
+    )
 };
 
 export default Form;
