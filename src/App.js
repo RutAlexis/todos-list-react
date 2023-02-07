6116f30d7e9d2a5e4538b14374b925c20af9f574
Aleksandra Rutkowska <adrabikm@gmail.com>
2023-02-07 08:32:22 +0100

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -29,6 +29,13 @@ function App() {
     }));
   };
 
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task,
+      done: true
+    })));
+  };
+
   return (
     <main className="container">
       <Header
@@ -55,6 +62,7 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
           />
         }
       />
