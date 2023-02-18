089fe4f54b7b4da025efa5039374c2e55b0b5e18
Aleksandra Rutkowska <adrabikm@gmail.com>
2023-02-18 16:21:13 +0100

    added inputRef

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
+import { useRef } from "react";
 import { Button, Input, FormStyled } from "./styled";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null);
 
     const onFromSubmit = (event) => {
         event.preventDefault();
+
         if (newTaskContent.trim() === "") {
             return;
         }
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
+        inputRef.current.focus();
     };
     return (
         <FormStyled onSubmit={onFromSubmit}>
             <Input
+                ref={inputRef}
                 value={newTaskContent}
                 placeholder="Co jest do zrobienia?"
                 onChange={({ target }) => setNewTaskContent(target.value)}
